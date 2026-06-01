16f6638ee68082125b34731a36739e8d9d535de0
Vazol-zav <jaydenhayes2024@gmail.com>
2026-06-01 17:00:09 -0400

    Add Pen class for drawing with mouse movement

--- assets/js/draw.js
+++ assets/js/draw.js
@@ -1,1 +1,16 @@
-
+class Pen {
+  constructor(x, y) {
+  this.x = x;
+  this.y = y;
+  this.lastX = x;
+  this.lastY = y;
+  this.angle = Math.random() * Math.PI * 2;
+  this.speed = 2 + Math.random() * 2;
+  this.color = `hsl(${Math.random()*360}, 100%, 50%)`;
+  this.size = 4;
+}
+document.addEventListener("mousemove", (e) => {
+  for (let i = 0; i < 3; i++) {
+    pens.push(new Pen(e.clientX, e.clientY));
+  }
+});
