4f99e4f140b74c3c690afcf6a201642ea419f919
Vazol-zav <jaydenhayes2024@gmail.com>
2026-06-01 15:47:14 -0400

    Implement orb movement on mouse move
    Add mouse movement functionality to the orb element.

--- assets/js/orb.js
+++ assets/js/orb.js
@@ -1,1 +1,6 @@
+/*this is the code to move the orb/ new mouse.*/
+const orb = document.querySelector(".orb");
 
+
+document.addEventListener("mousemove", (event) => {
+  orb.style.transform = `translate(${event.clientX-20}px, ${event.clientY-20}px)`;
