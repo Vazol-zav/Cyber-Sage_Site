a04d106432e99c201f232f1b6e72134386daeaa8
Vazol-zav <jaydenhayes2024@gmail.com>
2026-06-01 16:35:17 -0400

    Refactor orb movement to use left and top styles
    THE GLOURIOUS ORB IS HERE XD XD XD

--- assets/js/orb.js
+++ assets/js/orb.js
@@ -1,6 +1,6 @@
 /*this is the code to move the orb/ new mouse.*/
 const orb = document.querySelector(".orb");
-
-
-document.addEventListener("mousemove", (event) => {
-  orb.style.transform = `translate(${event.clientX-20}px, ${event.clientY-20}px)`;});
+document.addEventListener("mousemove", (move) => {
+  orb.style.left=(move.clientX-20)+"px";
+  orb.style.top=(move.clientY-20)+"px";
+})
