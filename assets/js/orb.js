1e689db527d101128cd8b7f8410843c8d40f1d8f
Vazol-zav <jaydenhayes2024@gmail.com>
2026-06-02 10:51:08 -0400

    Add ghost spawning effect on mouse movement

--- assets/js/orb.js
+++ assets/js/orb.js
@@ -4,3 +4,23 @@ document.addEventListener("mousemove", (move) => {
   orb.style.left=(move.clientX-20)+"px";
   orb.style.top=(move.clientY-20)+"px";
 })
+function spawnGhost(x, y) {
+  const ghost = document.createElement("div");
+  ghost.className = "ghost";
+  ghost.style.left = x + "px";
+  ghost.style.top = y + "px";
+
+  document.body.appendChild(ghost);
+
+  // fade + remove
+  requestAnimationFrame(() => {
+    ghost.style.opacity = "0";
+    ghost.style.transform = "translate(-50%, -50%) scale(1.4)";
+  });
+
+  setTimeout(() => ghost.remove(), 400);
+}
+
+document.addEventListener("mousemove", (e) => {
+  spawnGhost(e.clientX, e.clientY);
+});
